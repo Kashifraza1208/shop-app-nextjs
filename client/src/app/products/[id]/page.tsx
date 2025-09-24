5eed552c1b3e72a3a44357ba9e65e5858b331d7a
Md Kashif Raza <kashifrazasonbarsa@gmail.com>
2025-09-24 21:44:16 +0530

    added admin and user folder

---
+++ client/src/app/products/[id]/page.tsx
@@ -0,0 +1,215 @@
+"use client";
+import ProductInteraction from "@/components/ProductInteraction";
+import { ProductsType, ProductType } from "@/types";
+import Image from "next/image";
+import { useMemo } from "react";
+
+const products: ProductsType = [
+  {
+    id: 1,
+    name: "Adidas CoreFit T-Shirt",
+    shortDescription:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 39.9,
+    sizes: ["s", "m", "l", "xl", "xxl"],
+    colors: ["gray", "purple", "green"],
+    images: {
+      gray: "/products/1g.png",
+      purple: "/products/1p.png",
+      green: "/products/1gr.png",
+    },
+  },
+  {
+    id: 2,
+    name: "Puma Ultra Warm Zip",
+    shortDescription:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 59.9,
+    sizes: ["s", "m", "l", "xl"],
+    colors: ["gray", "green"],
+    images: { gray: "/products/2g.png", green: "/products/2gr.png" },
+  },
+  {
+    id: 3,
+    name: "Nike Air Essentials Pullover",
+    shortDescription:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 69.9,
+    sizes: ["s", "m", "l"],
+    colors: ["green", "blue", "black"],
+    images: {
+      green: "/products/3gr.png",
+      blue: "/products/3b.png",
+      black: "/products/3bl.png",
+    },
+  },
+  {
+    id: 4,
+    name: "Nike Dri Flex T-Shirt",
+    shortDescription:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 29.9,
+    sizes: ["s", "m", "l"],
+    colors: ["white", "pink"],
+    images: { white: "/products/4w.png", pink: "/products/4p.png" },
+  },
+  {
+    id: 5,
+    name: "Under Armour StormFleece",
+    shortDescription:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 49.9,
+    sizes: ["s", "m", "l"],
+    colors: ["red", "orange", "black"],
+    images: {
+      red: "/products/5r.png",
+      orange: "/products/5o.png",
+      black: "/products/5bl.png",
+    },
+  },
+  {
+    id: 6,
+    name: "Nike Air Max 270",
+    shortDescription:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 59.9,
+    sizes: ["40", "42", "43", "44"],
+    colors: ["gray", "white"],
+    images: { gray: "/products/6g.png", white: "/products/6w.png" },
+  },
+  {
+    id: 7,
+    name: "Nike Ultraboost Pulse ",
+    shortDescription:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 69.9,
+    sizes: ["40", "42", "43"],
+    colors: ["gray", "pink"],
+    images: { gray: "/products/7g.png", pink: "/products/7p.png" },
+  },
+  {
+    id: 8,
+    name: "Levi’s Classic Denim",
+    shortDescription:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    description:
+      "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+    price: 59.9,
+    sizes: ["s", "m", "l"],
+    colors: ["blue", "green"],
+    images: { blue: "/products/8b.png", green: "/products/8gr.png" },
+  },
+];
+
+export const generateMetaData = async ({
+  params,
+}: {
+  params: { id: string };
+}) => {
+  const product: ProductType | null = useMemo(() => {
+    const id = parseInt(params.id);
+    return products.find((p) => p.id === id) || null;
+  }, [params.id]);
+
+  if (!product) {
+    return <div className="mt-12 text-red-500">Product not found</div>;
+  }
+
+  return {
+    title: product.name,
+    description: product.description,
+  };
+};
+
+const ProductPage = ({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { color?: string; size?: string };
+}) => {
+  const product: ProductType | null = useMemo(() => {
+    const id = parseInt(params.id);
+    return products.find((p) => p.id === id) || null;
+  }, [params.id]);
+
+  if (!product) {
+    return <div className="mt-12 text-red-500">Product not found</div>;
+  }
+
+  const selectedSize = searchParams.size || product.sizes[0];
+  const selectedColor = searchParams.color || product.colors[0];
+
+  return (
+    <div className="flex flex-col gap-4 lg:flex-row md:gap-12 mt-12">
+      {/* IMAGE */}
+      <div className="w-full lg:w-5/12 relative aspect-[2/3]">
+        <Image
+          src={product.images[selectedColor]}
+          alt={product.name}
+          fill
+          className="object-contain rounded-md"
+        />
+      </div>
+      {/* DETAILS */}
+      <div className="w-full lg:w-7/12 flex flex-col gap-4">
+        <h1 className="text-2xl font-medium">{product.name}</h1>
+        <p className="text-gray-500">{product.description}</p>
+        <h2 className="text-2xl font-semibold">${product.price.toFixed(2)}</h2>
+        <ProductInteraction
+          product={product}
+          selectedSize={selectedSize}
+          selectedColor={selectedColor}
+        />
+        {/* CARD INFO */}
+        <div className="flex items-center gap-2 mt-4">
+          <Image
+            src="/klarna.png"
+            alt="klarna"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+          <Image
+            src="/cards.png"
+            alt="cards"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+          <Image
+            src="/stripe.png"
+            alt="stripe"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+        </div>
+        <p className="text-gray-500 text-xs">
+          By clicking Pay Now, you agree to our{" "}
+          <span className="underline hover:text-black">Terms & Conditions</span>{" "}
+          and <span className="underline hover:text-black">Privacy Policy</span>
+          . You authorize us to charge your selected payment method for the
+          total amount shown. All sales are subject to our return and{" "}
+          <span className="underline hover:text-black">Refund Policies</span>.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
